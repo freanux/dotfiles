d0661b396dd7ae03630fb2ecdb9e6c71a8ef21da
freanux <pw@earthwave.ch>
2023-02-28 20:51:31 +0100

    prev/next diagnostics to TAB, S-TAB

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -18,11 +18,14 @@ lsp.configure('lua_ls', {
     }
 })
 
+--[[
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<PageUp>'] = cmp.mapping.scroll_docs(-15),
+  ['<PageDown>'] = cmp.mapping.scroll_docs(15),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
@@ -33,6 +36,7 @@ cmp_mappings['<S-Tab>'] = nil
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
+--]]
 
 lsp.set_preferences({
     suggest_lsp_servers = false,
@@ -51,8 +55,10 @@ lsp.on_attach(function(_, bufnr)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  --vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  --vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<Tab>", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "<S-Tab>", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
