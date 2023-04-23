688831529924d5c3d510e4bd23e1d813736cca70
freanux <pw@earthwave.ch>
2023-04-23 23:01:40 +0200

    telescope lsp added

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -62,27 +62,17 @@ lsp.on_attach(function(_, bufnr)
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  --vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  --vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   --vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   --vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<Tab>", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "<S-Tab>", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  --vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  --vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  --vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
-
-local wk = require("which-key")
-wk.register({
-    v = {
-        name = "LSP",
-        w = { name = "Workspace" },
-        c = { name = "Code" },
-        r = { name = "Reference" },
-    },
-}, { prefix = "<leader>" })
 
 lsp.setup()
 
--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,21 +1,57 @@
 local builtin = require('telescope.builtin')
-local wk = require("which-key")
 
+local wk = require("which-key")
 wk.register({
     f = {
         name = "Find",
         f = { builtin.find_files, "Find File" },
         g = { builtin.live_grep, "Live Grep" },
-        b = { builtin.buffers, "Buffers" },
-        h = { builtin.help_tags, "Help" },
+        b = { builtin.buffers, "Find in Buffers" },
+        h = { builtin.help_tags, "Find in Help" },
+        d = { builtin.diagnostics, "Find in Diagnostics" },
+        s = { function() builtin.grep_string({ search = vim.fn.input("Grep> ") }) end, "Grep String" },
     },
     g = {
-        name = "Grep",
-        g = { function() builtin.grep_string({ search = vim.fn.input("Grep> ") }) end, "Grep String" },
+        name = "Git",
+        f = { builtin.git_files, "Git Files" },
+        c = { builtin.git_commits, "Git Commits" },
+        b = { builtin.git_branches, "Git Branches" },
+        s = { builtin.git_status, "Git Status" },
+        h = { builtin.git_stashes, "Git Stashes" },
     },
-}, { prefix = "<leader>" })
+    p = {
+        name = "Pickers",
+        r = { builtin.registers, "Registers" },
+        c = { builtin.commands, "Commands" },
+        k = { builtin.keymaps, "Keymaps" },
+        f = { builtin.filetypes, "File Types" },
+        j = { builtin.jumplist, "Jump List" },
+    },
+    l = {
+        name = "LSP",
+        r = { builtin.lsp_references, "References" },
+        i = { builtin.lsp_implementations, "Implementations" },
+        d = { builtin.lsp_definitions, "Definitions" },
+        t = { builtin.lsp_type_definitions, "Type Definitions" },
+        g = { builtin.diagnostics, "Diagnostics" },
+        s = {
+            name = "Symbols",
+            w = { builtin.lsp_workspace_symbols, "Workspace Symbols" },
+            d = { builtin.lsp_document_symbols, "Document Symbols" },
+            y = { builtin.lsp_dynamic_workspace_symbols, "Dynamic Workspace Symbols" },
+        },
+        c = {
+            name = "Calls",
+            i = { builtin.lsp_incoming_calls, "Incoming Calls" },
+            o = { builtin.lsp_outgoing_calls, "Outgoing Calls" },
+        },
+    },
+}, { prefix = "<leader>"})
+
+vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "Find in Buffers" })
 
 local actions = require("telescope.actions")
+
 require("telescope").setup({
     defaults = {
         mappings = {
