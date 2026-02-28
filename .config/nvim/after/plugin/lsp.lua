14fac82242b1b2766935589741efd3b3c44dab77
freanux <pw@earthwave.ch>
2026-02-28 14:52:30 +0100

    lsp.lua 'fixed'

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -8,8 +8,8 @@ require('mason-lspconfig').setup({
     handlers = {
         lsp.default_setup,
         lua_ls = function()
-            local lua_opts = lsp.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
+--            local lua_opts = lsp.nvim_lua_ls()
+--            require('lspconfig').lua_ls.setup(lua_opts)
         end,
     }
 })
